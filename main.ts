407bb2751af8427b501e28778eeb85568c0ed064
Tim Rogers <me@timrogers.co.uk>
2022-06-07 09:01:29 +0100

    Configure and apply Prettier

--- main.ts
+++ main.ts
@@ -1,12 +1,20 @@
-import { App, Editor, MarkdownView, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+  App,
+  Editor,
+  MarkdownView,
+  Notice,
+  Plugin,
+  PluginSettingTab,
+  Setting,
+} from 'obsidian';
 import { Octokit } from 'octokit';
 interface ShareAsGistSettings {
-	dotcomPersonalAccessToken: string | null;
+  dotcomPersonalAccessToken: string | null;
 }
 
 const DEFAULT_SETTINGS: ShareAsGistSettings = {
-	dotcomPersonalAccessToken: null
-}
+  dotcomPersonalAccessToken: null,
+};
 
 interface CreateGistResult {
   status: 'succeeded' | 'failed';
@@ -15,17 +23,19 @@ interface CreateGistResult {
 }
 
 export default class ShareAsGistPlugin extends Plugin {
-	settings: ShareAsGistSettings;
+  settings: ShareAsGistSettings;
 
-	async onload() {
-		await this.loadSettings();
+  async onload() {
+    await this.loadSettings();
 
     this.addCommand({
       id: 'share-as-public-dotcom-gist',
       name: 'Create public link on GitHub.com',
       editorCallback: async (editor: Editor, view: MarkdownView) => {
         if (!this.settings.dotcomPersonalAccessToken) {
-          return new Notice('You need to add your GitHub personal access token in Settings.')
+          return new Notice(
+            'You need to add your GitHub personal access token in Settings.',
+          );
         }
 
         const content = editor.getValue();
@@ -41,7 +51,7 @@ export default class ShareAsGistPlugin extends Plugin {
         } else {
           new Notice(`GitHub API error: ${result.errorMessage}`);
         }
-      }
+      },
     });
 
     this.addCommand({
@@ -49,7 +59,9 @@ export default class ShareAsGistPlugin extends Plugin {
       name: 'Create private link on GitHub.com',
       editorCallback: async (editor: Editor, view: MarkdownView) => {
         if (!this.settings.dotcomPersonalAccessToken) {
-          return new Notice('You need to add your GitHub personal access token in Settings.')
+          return new Notice(
+            'You need to add your GitHub personal access token in Settings.',
+          );
         }
 
         const content = editor.getValue();
@@ -63,72 +75,82 @@ export default class ShareAsGistPlugin extends Plugin {
         } else {
           new Notice(`GitHub API error: ${result.errorMessage}`);
         }
-      }
+      },
     });
-		
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new ShareAsGistSettingTab(this.app, this));
-	}
 
-  async createGist(filename: string, content: string, isPublic: boolean): Promise<CreateGistResult> {
+    // This adds a settings tab so the user can configure various aspects of the plugin
+    this.addSettingTab(new ShareAsGistSettingTab(this.app, this));
+  }
+
+  async createGist(
+    filename: string,
+    content: string,
+    isPublic: boolean,
+  ): Promise<CreateGistResult> {
     try {
-      const octokit = new Octokit({ auth: this.settings.dotcomPersonalAccessToken });
+      const octokit = new Octokit({
+        auth: this.settings.dotcomPersonalAccessToken,
+      });
 
       const response = await octokit.rest.gists.create({
         description: filename,
         public: isPublic,
         files: {
-          [filename]: { content }
-        }
+          [filename]: { content },
+        },
       });
 
       return {
         status: 'succeeded',
         url: response.data.html_url as string,
-        errorMessage: null
+        errorMessage: null,
       };
     } catch (e) {
       return {
         status: 'failed',
         url: null,
-        errorMessage: e.message
+        errorMessage: e.message,
       };
     }
   }
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
 }
 
 class ShareAsGistSettingTab extends PluginSettingTab {
-	plugin: ShareAsGistPlugin;
+  plugin: ShareAsGistPlugin;
 
-	constructor(app: App, plugin: ShareAsGistPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+  constructor(app: App, plugin: ShareAsGistPlugin) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
 
-	display(): void {
-		const {containerEl} = this;
+  display(): void {
+    const { containerEl } = this;
 
-		containerEl.empty();
+    containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Share as Gist'});
+    containerEl.createEl('h2', { text: 'Share as Gist' });
 
-		new Setting(containerEl)
-			.setName('GitHub.com personal access token')
-			.setDesc('An access token for GitHub.com with permission to write gists. You can create one from "Settings" in your GitHub account.')
-			.addText(text => text
-				.setPlaceholder('Your personal access token')
-				.setValue(this.plugin.settings.dotcomPersonalAccessToken)
-				.onChange(async (value) => {
-					this.plugin.settings.dotcomPersonalAccessToken = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+    new Setting(containerEl)
+      .setName('GitHub.com personal access token')
+      .setDesc(
+        'An access token for GitHub.com with permission to write gists. You can create one from "Settings" in your GitHub account.',
+      )
+      .addText((text) =>
+        text
+          .setPlaceholder('Your personal access token')
+          .setValue(this.plugin.settings.dotcomPersonalAccessToken)
+          .onChange(async (value) => {
+            this.plugin.settings.dotcomPersonalAccessToken = value;
+            await this.plugin.saveSettings();
+          }),
+      );
+  }
 }
